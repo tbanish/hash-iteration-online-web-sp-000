e84748515c50756051655cb7e10a296073b61d6d
Tom Banish <tombanish@gmail.com>
2019-11-29 01:12:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age > 12
+      puts "You are too old for this."
+    end
+  end
+end
